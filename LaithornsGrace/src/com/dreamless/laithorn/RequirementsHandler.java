7c7dd24a927b64db04833bf6e9b5f91599b6f240
dreamlessmemory <centurion475@gmail.com>
2019-04-14 17:56:56 -0400

    Rename, added flag requirements

--- LaithornsGrace/src/com/dreamless/laithorn/RequirementsHandler.java
+++ LaithornsGrace/src/com/dreamless/laithorn/RequirementsHandler.java
@@ -1,27 +1,33 @@
 package com.dreamless.laithorn;
 
 import java.util.HashMap;
+import java.util.List;
 
 import org.bukkit.entity.Player;
 
 import com.dreamless.laithorn.player.CacheHandler;
 import com.dreamless.laithorn.player.PlayerData;
 
-public class RecipeHandler {
+public class RequirementsHandler {
 
 	private static HashMap<String, Integer> levelRequirements = new HashMap<String, Integer>();
-	
+
 	public static void registerRecipe(String name, int level) {
 		levelRequirements.put(name, level);
 	}
-	
-	public static boolean canDoAction(Player player, String action) {
-		
-		if(!levelRequirements.containsKey(action)){
+
+	public static boolean canDoAction(Player player, String action, List<String> flags) {
+
+		if (!levelRequirements.containsKey(action)) {
 			return false;
 		}
-		
+
 		PlayerData data = CacheHandler.getPlayer(player);
-		return data.getSmithingLevel() >= levelRequirements.get(action); 
+
+		for (String flag : flags) {
+			if(!data.getFlag(flag))
+				return false;
+		}
+		return data.getSmithingLevel() >= levelRequirements.get(action);
 	}
 }
